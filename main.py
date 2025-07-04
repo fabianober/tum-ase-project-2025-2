a40b969fa5f58ed508369c3b75602b2ce12351b5
YannisEndr <yannis@endress-hg.de>
2025-07-04 18:33:03 +0200

    Already adjusted some parameters and defined some constants

---
+++ main.py
@@ -0,0 +1,10 @@
+'Start by defining some of the necessary dimensions'
+#Stacking sequences 
+panelStack=[45,45,-45,-45,0,0,90,90,90,90,0,0,-45,-45,45,45]
+StringerFlange=[45,45,-45,-45,0,0,90,90,90,90,0,0,-45,-45,45,45]
+StringerWeb=[-45,-45,45,45,0,0,90,90,90,90,0,0,45,45,-45,-45]
+
+#Ply thicknesses 
+tPanel = 0.552
+tStringer = 0.25
+
