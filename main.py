07378e419b00f649f7fcdddb81ea5f3da87e0429
Fabian Ober <fabian@familie-ober.eu>
2025-07-05 20:44:21 +0200

    add personal data parser

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import os
 import random
 import string
+import json
 
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('hmscript'))
@@ -10,7 +11,18 @@ sys.path.insert(0, os.path.abspath('calculators'))
 
 from abd_matrix import *
 
+
 print("Starting analysis...")
+
+with open('data.json', 'r') as f:
+    personal_data = json.load(f)
+
+name = personal_data.get("name")
+E_11_avg = personal_data.get("E_11_avg")
+E_22_avg = personal_data.get("E_22_avg")
+G_12_avg = personal_data.get("G_12_avg")
+
+print(f"Your data is: Name: {name}, E_11_avg: {E_11_avg}, E_22_avg: {E_22_avg}, G_12_avg: {G_12_avg}")
 
 'Start by defining some of the necessary dimensions'
 #Stacking sequences 
