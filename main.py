b0ea11845ed3eb1e6b9209efe7674300ca4b77ef
YannisEndr <yannis@endress-hg.de>
2025-07-05 21:17:20 +0200

    Added ABD for all components

--- main.py
+++ main.py
@@ -34,7 +34,9 @@ StringerWeb=[-45,-45,45,45,0,0,90,90,90,90,0,0,45,45,-45,-45]
 tPanel = 0.552
 tStringer = 0.25
 
-ABD, ABD_inverse = calculateABD(stacksequence=panelStack, plyT=tPanel, EModulus1=E_11_avg, EModulus2=E_22_avg, ShearModulus=G_12_avg)
-print(ABD)
-print(ABD_inverse)
+ABD_panel, ABD_panel_inverse = calculateABD(stacksequence=panelStack, plyT=tPanel, EModulus1=E_11_avg, EModulus2=E_22_avg, ShearModulus=G_12_avg)
+ABD_flange, ABD_flange_inverse = calculateABD(stacksequence=StringerFlange, plyT=tStringer, EModulus1=E_11_avg, EModulus2=E_22_avg, ShearModulus=G_12_avg)
+ABD_flange, ABD_flange_inverse = calculateABD(stacksequence=StringerWeb, plyT=tStringer, EModulus1=E_11_avg, EModulus2=E_22_avg, ShearModulus=G_12_avg)
+print(ABD_panel)
+print(ABD_panel_inverse)
 
