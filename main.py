63a4a6707af9241b58142cf2c078ab89c6c1f7ff
HelopilotR <Felix_Resch@gmx.de>
2025-07-10 14:36:28 +0200

    Simple cleanup in main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import os
-import random
-import string
+#import random #(Unused import)
+#import string #(Unused import)
 import json
 
 sys.path.insert(0, os.path.abspath('..'))
@@ -24,7 +24,7 @@ G_12_avg = personal_data.get("G_12_avg")
 
 print(f"Your data is: Name: {name}, E_11_avg: {E_11_avg}, E_22_avg: {E_22_avg}, G_12_avg: {G_12_avg}")
 
-'Start by defining some of the necessary dimensions'
+#'Start by defining some of the necessary dimensions'
 #Stacking sequences 
 panelStack=[45,45,-45,-45,0,0,90,90,90,90,0,0,-45,-45,45,45]
 StringerFlange=[45,45,-45,-45,0,0,90,90,90,90,0,0,-45,-45,45,45]
